cb8dad2cff3df461d34ed5fa2937777e0df5e71b
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-08 02:56:21 +0000

    Added code to Redirect any request to '/u/:shortURL' to its longURL

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Function to generate a random string of input length to be used as shortURL
 const generateRandomString = function(length) {
   const characters = 'ABCDEFGHIKJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = "";
@@ -17,35 +18,43 @@ const generateRandomString = function(length) {
 //Set EJS as the view engine
 app.set("view engine", "ejs");
 
+//Object to maintain URL Data
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+//Handle GET request to path /urls
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+//Handle GET request to path /urls/new , renders the form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Handle GET request to path /urls/:shortURL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+//Handle GET request to path /u/:shortURL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//Handle POST request to path /urls
 app.post("/urls", (req, res) => {
   const newShortURL = generateRandomString(6);
   urlDatabase[newShortURL] = req.body.longURL;
   res.redirect(`/urls/${newShortURL}`);
 });
 
+//Server listening
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
